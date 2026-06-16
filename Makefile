8532413eae359b395c0138f8163ae984236b69da
Tristan <trist007@darkterminal.net>
2026-06-16 13:46:00 -0500

    implemented rotating vectors using linear transformations

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = C:/msys64/ucrt64/bin/gcc.exe
 CFLAGS = -IC:/sdl2/SDL2-2.30.11/include -IC:/msys64/ucrt64/include -Wall -std=c99 -Wno-unused-variable
 
-LIBS = -LC:/sdl2/SDL2-2.30.11/lib/x64 -lSDL2main -lSDL2
+LIBS = -LC:/sdl2/SDL2-2.30.11/lib/x64 -lSDL2main -lSDL2 -lm
 
 build:
 	$(CC) ./src/*.c $(CFLAGS) $(LIBS) -o renderer.exe
--- src/vector.h
+++ src/vector.h
@@ -1,26 +1,36 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-typedef struct
+typedef struct vec2_t vec2_t;
+struct vec2_t
 {
     float x;
     float y;
-} vec2_t;
+};
 
-typedef struct
+typedef struct vec3_t vec3_t;
+struct vec3_t
 {
     float x;
     float y;
     float z;
-} vec3_t;
+};
 
-typedef struct
+typedef struct camera_t camera_t;
+struct camera_t
 {
     vec3_t position;
     vec3_t rotation;
     float  fov_angle;
-} camera_t;
+};
 
 // ! TODO: trist007: matrices
+vec2_t project(vec3_t point);
+
+void transform_points(void);
+
+vec3_t vec3_rotate_x(vec3_t v, float angle);
+vec3_t vec3_rotate_y(vec3_t v, float angle);
+vec3_t vec3_rotate_z(vec3_t v, float angle);
 
 #endif // VECTOR_H
