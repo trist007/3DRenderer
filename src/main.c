f10ad032d130f59565934de7d58025ec5d9dda44
Tristan <trist007@darkterminal.net>
2026-09-14 14:19:34 -0500

    matrix mult order matters, Scale, then Rotation, then Translation

--- src/main.c
+++ src/main.c
@@ -105,17 +105,20 @@ void update(void) {
     triangles_to_render = NULL;
 
     // Change the mesh scale/rotation values per animation frame
-    // mesh.rotation.x += 0.01;
-    // mesh.rotation.y += 0.01;
-    // mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
     // mesh.scale.x += 0.002;
     // mesh.scale.y += 0.001;
     mesh.translation.x += 0.01;
     mesh.translation.z = 5.0;
 
-    // Create a scale and translatioon matrix that will be used to multiply the mesh vertices
+    // Create a scale, rotation, and translatioon matrix that will be used to multiply the mesh vertices
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.z);
     mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
 
     // Loop all triangle faces of our mesh
     int num_faces = array_length(mesh.faces);
@@ -133,8 +136,12 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-            // Use a matrix to scale our original vertex
+            // Perform Matrix Multiplication for scale, translation, and rotation
+            // NOTE(trist007): Order matters Scale first, rotate, then translate
             transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
             transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
             // Save transformed vertex in the array of transformed vertices
